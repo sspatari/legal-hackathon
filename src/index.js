1e650856f1474c1aed6d1e055d9172fd65c07673
sspatari <spatari.stanislav@gmail.com>
2018-12-08 21:04:18 +0200

    Solve ssl certification

--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 import 'babel-polyfill';
-// import axios from 'axios';
-// import sslRootCas from 'ssl-root-cas';
-// import https from 'https';
-//
-// https.globalAgent.options.ca = sslRootCas.create();
-//
-// axios('https://jc.instante.justice.md').then( res => {
-//   console.log(res.status);
-// }).catch( error => {
-//   console.log(error.message);
-// });
+import axios from 'axios';
 
-const rp = require('request-promise');
-const $ = require('cheerio');
-const url = 'https://jc.instante.justice.md';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-rp(url)
-  .then(function(html){
-    //success!
-    // const wikiUrls = [];
-    // for (let i = 0; i < 45; i++) {
-    //   wikiUrls.push($('big > a', html)[i].attribs.href);
-    // }
-    // console.log(wikiUrls);
-    console.log(html);
-  })
-  .catch(function(err){
-    console.log('Error' + err);
-    //handle error
-  });
+axios('https://jc.instante.justice.md').then( res => {
+  console.log(res.data);
+}).catch( error => {
+  console.log(error);
+});
